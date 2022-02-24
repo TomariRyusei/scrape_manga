187cbf4257602475af7452c71346f5b35568f206
TomariRyusei <t.ryu6635@gmail.com>
2022-02-24 16:43:15 +0900

    get and output arrival date

--- index.mjs
+++ index.mjs
@@ -12,11 +12,22 @@ const { JSDOM } = jsdom;
     const html = await res.text();
     const dom = new JSDOM(html);
     const document = dom.window.document;
-    const nodes = document.querySelectorAll(".arrival_detail ");
-    const childNodes = Array.from(nodes, (dd) => dd.childNodes);
+
+    // 漫画入荷日付を抽出
+    const arrivalDateNodes = document.querySelectorAll(".arrival_date");
+    // 入荷日付データ
+    const arrivalDateList = Array.from(arrivalDateNodes, (date) =>
+      date.textContent.trim()
+    );
 
     // 漫画のタイトルだけ抽出
-    const mangaTitles = childNodes.map((childNode) => {
+    const arrivalDetailNodes = document.querySelectorAll(".arrival_detail ");
+    const arrivalDetailChildNodes = Array.from(
+      arrivalDetailNodes,
+      (dd) => dd.childNodes
+    );
+    // 漫画タイトルデータ
+    const mangaTitleList = arrivalDetailChildNodes.map((childNode) => {
       let mangaTitle = "";
       childNode.forEach((elem, index) => {
         if (index === 1) {
@@ -25,7 +36,23 @@ const { JSDOM } = jsdom;
       });
       return mangaTitle;
     });
-    outputMangaInfo(mangaTitles.join("\n"));
+
+    // 件数は同じになるはず
+    if (arrivalDateList.length !== mangaTitleList.length)
+      throw new Error("漫画の入荷日付数とタイトル数が一致しません。");
+
+    // 日付とタイトルを結合
+    const outputData = mangaTitleList.map((mangaTitle, index) => {
+      let joinedData = "";
+      arrivalDateList.forEach((date, dateIndex) => {
+        if (index === dateIndex) {
+          joinedData = `${date} ${mangaTitle}`;
+        }
+      });
+      return joinedData;
+    });
+
+    outputMangaInfo(outputData.join("\n"));
   } catch (e) {
     outputLog(e.toString());
   }
